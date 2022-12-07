bcf2afa1e092922aec02eff9a5cfa15607239a1d
schlenkerc18 <schlenkerc18@hanover.edu>
2022-12-06 19:25:58 -0500

    Add project files.

---
+++ BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,26 @@
+﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository pieRepository;
+        private readonly ICategoryRepository categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            this.pieRepository = pieRepository;
+            this.categoryRepository = categoryRepository;
+        }
+
+        public IActionResult List()
+        {
+            //ViewBag.CurrentCategory = "Cheese cakes";
+            //return View(pieRepository.AllPies);
+            PieListViewModel piesListViewModel = new PieListViewModel(pieRepository.AllPies, "Cheese Cakes");
+            return View(piesListViewModel);
+        }
+    }
+}
---
+++ BethanysPieShop/Program.cs
@@ -0,0 +1,21 @@
+using BethanysPieShop.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+
+builder.Services.AddControllersWithViews(); // makes sure asp.net core app will know about mvc
+
+var app = builder.Build();
+
+app.UseStaticFiles();  // look in wwwroot folder for static files, shortcircuit request
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.MapDefaultControllerRoute(); // lets us see our pages
+
+app.Run(); // starts app
